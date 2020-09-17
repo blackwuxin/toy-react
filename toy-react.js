7676fc11f3ae33417aa91e70c02fce821b68e2e2
Johnny Wu （吴心翔） <wu_xx@trip.com>
2020-09-17 21:00:08 +0800

    update

--- toy-react.js
+++ toy-react.js
@@ -20,19 +20,73 @@ export class Component {
     }
     [RENDER_TO_DOM](range) {
         this._range = range;
-        this.render()[RENDER_TO_DOM](range)
+        this._vdom = this.vdom;
+        this._vdom[RENDER_TO_DOM](range)
     }
+    update(){
+        let isSameNode = (oldNode,newNode) =>{
+            if(oldNode.type !== newNode.type){
+                return false;
+            }
+            for(let name in newNode.props){
+                if(oldNode[name] !== newNode[name]){
+                    return false;
+                }
+            }
+            if(Object.keys(oldNode.props).length > Object.keys(newNode.props).length){
+                return false;
+            }
+            if(newNode.type === '#text'){
+                if(oldNode.content !== newNode.content){
+                    return false;
+                }
+            }
+            return true;
+        }
+        let update = (oldNode,newNode)=>{
+            if(!isSameNode(oldNode,newNode)){
+                newNode[RENDER_TO_DOM](oldNode._range);
+                return;
+            }
+            newNode._range = oldNode._range;
 
-    rerender() {
-        let oldRange = this._range;
-        let range = document.createRange();
-        range.setStart(oldRange.startContainer, oldRange.StartOffSet);
-        range.setEnd(oldRange.startContainer, oldRange.StartOffSet);
-        this[RENDER_TO_DOM](range);
+            let newChildren = newNode.vchildren;
+            let oldChildren = oldNode.vchildren;
 
-        oldRange.setStart(range.endContainer, range.endOffset);
-        oldRange.deleteContents();
+            if(!newChildren || !newChildren.length){
+                return;
+            }
+            let tailRange = oldChildren[oldChildren.length-1]._range;
+
+            for(let i=0;i<newChildren.length;i++){
+                let newChild = newChildren[i];
+                let oldChild = oldChildren[i];
+                if(i<oldChildren.length){
+                    update(oldChild,newChild);
+                }else{
+                    let range = document.createRange();
+                    range.setStart(tailRange.endContainer,tailRange.endOffset);
+                    range.setEnd(tailRange.endContainer,tailRange.endOffset);
+                    newChild[RENDER_TO_DOM](range);
+                    tailRange = range;
+                }
+            }
+
+        }
+        let vdom = this.vdom;
+        update(this._vdom,vdom);
+        this._vdom = vdom;
     }
+    // rerender() {
+    //     let oldRange = this._range;
+    //     let range = document.createRange();
+    //     range.setStart(oldRange.startContainer, oldRange.StartOffSet);
+    //     range.setEnd(oldRange.startContainer, oldRange.StartOffSet);
+    //     this[RENDER_TO_DOM](range);
+
+    //     oldRange.setStart(range.endContainer, range.endOffset);
+    //     oldRange.deleteContents();
+    // }
 
     setState(newState) {
         if (this.state === null || typeof this.state !== 'object') {
@@ -50,7 +104,8 @@ export class Component {
             }
         }
         merge(this.state, newState);
-        this.rerender();
+        // this.rerender();
+        this.update();
     }
 }
 class ElementWrapper extends Component {
